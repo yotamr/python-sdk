f1fc09e7294331ff1e2fc4c97052d7efd34b9820
yuvalb <yuval@alooma.io>
2016-04-20 15:17:28 +0300

    Fixes to setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,15 @@ from pip.req import parse_requirements
 install_reqs = parse_requirements("requirements.txt", session=False)
 
 reqs = [str(ir.req) for ir in install_reqs]
+sdk_package_name = 'alooma_pysdk'
 
 from distutils.core import setup
 
 setup(
-  name = 'alooma_pysdk',
-  packages = ['alooma_pysdk'],
-  version = '0.1.2',
+  name = sdk_package_name,
+  packages = [sdk_package_name],
+  package_data = {sdk_package_name: ['alooma_ca']},
+  version = '0.1.0.1',
   description = 'A Python SDK to embed in your Python app to send events to your Alooma server',
   url = 'https://github.com/Aloomaio/python-sdk',
   author = 'Yuval Barth',
