a625e4bf4d90017256d3ae4fc1849474fea27122
yuvalb <yuval@alooma.io>
2016-07-14 14:47:54 +0300

    Updated version in setup

--- setup.py
+++ setup.py
@@ -9,15 +9,15 @@ sdk_package_name = 'alooma_pysdk'
 from distutils.core import setup
 
 setup(
-  name = sdk_package_name,
-  packages = [sdk_package_name],
-  package_data = {sdk_package_name: ['alooma_ca']},
-  version = '1.0',
-  description = 'An easy-to-integrate SDK for your Python apps to report events to Alooma',
-  url = 'https://github.com/Aloomaio/python-sdk',
-  author = 'Yuval Barth',
-  author_email = 'yuval@alooma.com',
-  keywords = ['python', 'sdk', 'alooma', 'pysdk'],
-  install_requires = install_reqs
+    name=sdk_package_name,
+    packages=[sdk_package_name],
+    package_data={sdk_package_name: ['alooma_ca']},
+    version='2.0',
+    description='An easy-to-integrate SDK for your Python apps to report '
+                'events to Alooma',
+    url='https://github.com/Aloomaio/python-sdk',
+    author='Yuval Barth',
+    author_email='yuval@alooma.com',
+    keywords=['python', 'sdk', 'alooma', 'pysdk'],
+    install_requires=install_reqs
 )
-
